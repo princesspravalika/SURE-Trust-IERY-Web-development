95c7d1e4bbb613dc7262411cd7daf1fe43a6cf89
Mounika-sikakollu <115640311+Mounika-sikakollu@users.noreply.github.com>
2023-05-22 01:23:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,25 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
-
+let modalBtns = [...document.querySelectorAll(".button")];
+modalBtns.forEach(function (btn) {
+  btn.onclick = function () {
+    let modal = btn.getAttribute("data-modal");
+    document.getElementById(modal).style.display = "block";
+  };
+});
+let closeBtns = [...document.querySelectorAll(".close")];
+closeBtns.forEach(function (btn) {
+  btn.onclick = function () {
+    let modal = btn.closest(".modal");
+    modal.style.display = "none";
+  };
+});
+window.onclick = function (event) {
+  if (event.target.className === "modal") {
+    event.target.style.display = "none";
+  }
+};
 let slideIndex = 1;
 showSlides(slideIndex);
 
